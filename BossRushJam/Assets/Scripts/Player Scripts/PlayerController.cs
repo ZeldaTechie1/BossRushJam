1b9160e1ae8fd67edbdd684d5a9e24eb2e20dba9
Zeldatechie <jesrey4@outlook.com>
2023-01-23 21:31:13 -0500

    GJAM-39 #comment Added basic player movement, TODO add slide

--- BossRushJam/Assets/Scripts/Player Scripts/PlayerController.cs
+++ BossRushJam/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]private Camera _mainCamera;
+    [SerializeField]private float _movementSpeed;
+
+    private Rigidbody _rb;
+    private Vector3 _movementDirection;
+    private Vector2 _movementInput;
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    //this gets called by the Player Input Component on this object
+    public void OnLocomotion(InputValue value)
     {
-        
+       _movementInput = value.Get<Vector2>();
     }
+
+    private void FixedUpdate()
+    {
+        Vector3 currentOrientation = _mainCamera.transform.forward;
+        currentOrientation.y = 0;
+        currentOrientation.Normalize();
+        _movementDirection = (this.transform.forward * _movementInput.y) + (this.transform.right * _movementInput.x);
+        this.transform.forward = currentOrientation;
+        _rb.velocity = _movementDirection * _movementSpeed;
+    }
+
 }
