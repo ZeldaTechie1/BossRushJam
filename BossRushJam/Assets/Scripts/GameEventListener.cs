c9bb618a34f6181c7680f4d50ab8c1b6b2df760b
Jesus Reyes <jesrey4@outlook.com>
2023-01-24 21:40:57 -0500

    Development (#2)
    * GJAM-45 #in-progress #comment Began working on Enemy Behaviour
    
    * GJAM-45 #comment Feature implemented, testing required
    
    * GJAM-45 #comment Added modular event system, installed DOTween and confirmed testing for enemy wave system
    
    * GJAM-45 #implemented #comment Event system should be good to go!
    
    * GJAM-52 #in-progress #comment Began working on Enemy Class
    
    * GJAM-52 #comment Enemies now move towards the player, will work on attacking next
    
    * GJAM-45 #comment Enemies can now detect when they have hurt the player and will send the message
    
    * GJAM-52
    
    #comment Enemies are now able to take damage, waves finish and can continue on to the next wave. Enemies can also attack the player.
    
    * GJAM-52
    
    #implemented
    
    * #in-progress initial commit for this branch
    
    (testing if cards move without mentioning them in the commit)
    
    * GJAM-39 #in-progress
    
    * GJAM-39 #comment Added basic player movement, TODO add slide
    
    * Implemented player movement (#1)
    
    * Working on slide
    
    * GJAM-39 #comment Added slide and cooldown, TODO: add invulnerability
    
    * GJAM-39 #implemented #comment Finished player movement along with invulnerability

---
+++ BossRushJam/Assets/Scripts/GameEventListener.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CustomGameEvent : UnityEvent<Component, object> {}
+
+public class GameEventListener : MonoBehaviour
+{
+
+    [Tooltip("Event to register with.")]
+    public List<GameEvent> gameEvents;
+
+    [Tooltip("Response to invoke when Event with GameData is raised.")]
+    public List<UnityEvent<Component,object>> responses;
+
+    private void OnEnable() {
+        foreach(GameEvent gameEvent in gameEvents)
+        {
+            gameEvent.RegisterListener(this);
+        }
+    }
+
+    private void OnDisable() {
+        foreach (GameEvent gameEvent in gameEvents)
+        {
+            gameEvent.UnregisterListener(this);
+        }
+    }
+
+    public void OnEventRaised(Component sender, object data, GameEvent gameEvent) {
+        int gameEventIndex = gameEvents.IndexOf(gameEvent);
+        responses[gameEventIndex].Invoke(sender,data);
+    }
+
+}
