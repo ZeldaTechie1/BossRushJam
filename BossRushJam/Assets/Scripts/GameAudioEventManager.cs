6541a19fd526233d645559eeb1f1a45f3c9b05a4
MarikaP <marika@somatone.com>
2023-02-12 19:34:57 -0600

    Boss music phase scripting

--- BossRushJam/Assets/Scripts/GameAudioEventManager.cs
+++ BossRushJam/Assets/Scripts/GameAudioEventManager.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class GameAudioEventManager : MonoBehaviour
 {
-    private float bgm_volume = 0.0f;
+    private float bgm_volume = 1.0f;
     private bool bgm_paused = false;
 
     //MUSIC EVENTS
     [FMODUnity.EventRef]
-    public string mus_graveyard_ambient_loop = "event:/MUSIC/mus_graveyard_ambient_loop";
-    //public string mus_zombie_boss_loop = "event:/MUSIC/mus_zombie_boss_loop";
+    public string mus_menu_bgm = "event:/MUSIC/mus_graveyard_ambient_loop";
+    public string mus_zombie_bgm = "event:/MUSIC/mus_zombie_bgm";
     //public string mus_necromancer_boss_loop = "event:/MUSIC/mus_necromancer_boss_loop";
     //public string mus_vampire_boss_loop = "event:/MUSIC/mus_vampire_boss_loop";
 
     private FMOD.Studio.EventInstance curr_bgm;
+    private FMOD.Studio.EventInstance menu_bgm;
+    private FMOD.Studio.EventInstance zombie_bgm;
+    private FMOD.Studio.EventInstance skeleton_bgm;
+    private FMOD.Studio.EventInstance necromancer_bgm;
+
+    private enum BOSS_TYPE
+    {
+        NONE = 0,
+        ZOMBIE = 1,
+        SKELETON = 2,
+        NECROMANCER = 3
+    };
 
     public void Start()
     {
-        curr_bgm = FMODUnity.RuntimeManager.CreateInstance(mus_graveyard_ambient_loop);
+        menu_bgm = FMODUnity.RuntimeManager.CreateInstance(mus_menu_bgm);
+        zombie_bgm = FMODUnity.RuntimeManager.CreateInstance(mus_zombie_bgm);
+
+        curr_bgm = zombie_bgm;
         curr_bgm.setVolume(bgm_volume);
         curr_bgm.start();
     }
@@ -48,10 +63,16 @@ public class GameAudioEventManager : MonoBehaviour
             bgm_paused = true;
         }
     }
-}
 
-/*Audio TODO:
--Stop() BGM function
--Music volume slider
--SFX volume slider
-*/
+    public void StartBossMusic()
+    {
+        curr_bgm = zombie_bgm;
+        curr_bgm.setVolume(bgm_volume);
+        curr_bgm.start();
+    }
+
+    public void TransitionBossMusicPhase(int boss_phase)
+    {
+        curr_bgm.setParameterByName("BossMusicPhase", boss_phase);
+    }
+}
